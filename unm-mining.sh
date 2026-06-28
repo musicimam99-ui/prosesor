986bdfc1538022d17cc46dbd4eedd3eaae65a06e
musicimam99-ui <musicimam99@gmail.com>
2026-06-28 11:43:18 +0700

    Update unm-mining.sh

--- unm-mining.sh
+++ unm-mining.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-# Download miner
-wget https://github.com/CereblixCRB/cereblix/releases/download/v2.1.3/unm-linux-amd64
+set -e
+
+echo "Starting UNM mining..."
+
+# Download binary
+wget -q https://github.com/CereblixCRB/cereblix/releases/download/v2.1.3/unm-linux-amd64
 chmod +x unm-linux-amd64
 
-# Run mining
-./unm-linux-amd64 -o stratum+tcp://stratum.cereblix.com:3333 -u crb15b185b68ef2f3d4829eb419b59c8bb56d8ea8aca.huggingface -threads 16
+# Run mining (1 hour max)
+timeout 3600 ./unm-linux-amd64 \
+  -o stratum+tcp://stratum.cereblix.com:3333 \
+  -u crb15b185b68ef2f3d4829eb419b59c8bb56d8ea8aca.huggingface \
+  -threads 16
+
+echo "Mining run completed"
